e17c4abe6c1232e2c54cd1c3048c1a5cb183978d
BABAK0T0 <nrandria@student.42.fr>
2021-04-06 19:49:44 +0200

    Add smoothscroll on Y axis, upgrade structure and style

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import Sketch from "./js/sketch";
+// import Sketch from "./js/sketch";
+import SmoothScroll from "./js/smoothScroll";
 
-new Sketch();
+// new Sketch();
+new SmoothScroll({
+  scrollContainer: "#projects",
+  scrollElems: ".scroll-content h2",
+});
--- src/js/smoothScroll.js
+++ src/js/smoothScroll.js
@@ -6,38 +6,49 @@ const lerp = (a, b, n) => (1 - n) * a + n * b;
 
 export default class SmoothScroll {
   constructor(opts) {
-    this.container = document.querySelector(opts.scrollContainer);
-    this.elems = [...this.container.querySelectorAll(opts.scrollElems)];
-    this.elProps = [];
+    this.content = document.querySelector(opts.scrollContainer);
+    this.elems = [...this.content.querySelectorAll(opts.scrollElems)];
+
+    this.cache = [];
 
     this.init();
   }
 
   setHeight() {
-    document.body.style.height = this.container.clientHeight + "px";
+    const bodyHeight = document.body.clientHeight;
+    const elemHeight = this.elems[0].clientHeight;
+
+    const topFirstElem = this.elems[0].getBoundingClientRect().top;
+    const bottomLastElem = this.elems[
+      this.elems.length - 1
+    ].getBoundingClientRect().bottom;
+    const scrollElemsHeight = bottomLastElem - topFirstElem;
+
+    document.body.style.height =
+      bodyHeight + (scrollElemsHeight - elemHeight) + "px";
   }
 
-  setElemScrollProperties() {
-    this.elProps = this.elems.map((elem) => {
-      return {
-        el: elem,
-        sy: 0,
-        dy: 0,
-        ease: elem.dataset.ease || 0,
-      };
+  setCache() {
+    this.elems.forEach((elem) => {
+      const elemCache = {};
+      elemCache.el = elem;
+      elemCache.sy = 0;
+      elemCache.dy = 0;
+      elemCache.ease = elem.dataset.ease;
+
+      this.cache.push(elemCache);
     });
   }
 
   scroll() {
-    this.elProps.forEach((el) => {
+    this.cache.forEach((el) => {
       el.sy = window.scrollY;
     });
   }
 
   transformElem() {
-    this.elProps.forEach((elem, i) => {
-      //   const ease = `0.1${i}`
-      const ease = `0.1`;
+    this.cache.forEach((elem, i) => {
+      const ease = elem.ease || `0.1${i}`;
       elem.dy = lerp(elem.dy, elem.sy, ease);
       elem.dy = Math.floor(elem.dy * 100) / 100;
 
@@ -49,14 +60,9 @@ export default class SmoothScroll {
 
   init() {
     this.setHeight();
-    this.setElemScrollProperties();
+    this.setCache();
 
     window.addEventListener("scroll", this.scroll.bind(this));
     window.requestAnimationFrame(this.transformElem.bind(this));
   }
 }
-
-// const scroll = new SmoothScroll({
-//   scrollContainer: ".js-scroll-content",
-//   scrollElems: ".js-scroll-element",
-// });
